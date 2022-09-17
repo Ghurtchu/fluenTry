1d5f6412520423c5eda0761c125cf4ee4869839f
nghurtch <nghurtch@broadinstitute.com>
2022-09-17 19:27:58 +0400

    add more unit tests for all combinatorial cases

--- src/main/java/org/ghurtchu/protocols/PureCatchableMappable.java
+++ src/main/java/org/ghurtchu/protocols/PureCatchableMappable.java
@@ -3,6 +3,7 @@ package org.ghurtchu.protocols;
 import java.util.function.Function;
 
 public interface PureCatchableMappable<T> {
+    
     <V> V ifThrowsThenGetDefaultOrElseMap(
             Function<? super T, ? extends V> successMapper,
             V defaultValue,
--- src/main/java/org/ghurtchu/protocols/PureFoldable.java
+++ src/main/java/org/ghurtchu/protocols/PureFoldable.java
@@ -3,6 +3,7 @@ package org.ghurtchu.protocols;
 import java.util.function.Function;
 
 public interface PureFoldable<T> {
+
     <V> V fold(
             Function<? super T, ? extends V> successMapper,
             V defaultValue
