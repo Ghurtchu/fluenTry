384202fc4f5f03df9b5ab46571931e885fd1e480
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2022-12-16 23:49:14 +0400

    add gitignore

--- src/main/java/org/ghurtchu/Main.java
+++ src/main/java/org/ghurtchu/Main.java
@@ -2,11 +2,20 @@ package org.ghurtchu;
 
 import org.ghurtchu.Try.Try;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Main {
 
     public static void main(String[] args) {
 
-        Try.of(() -> 5 / 0).ifThrowsThenRunTask(System.out::println, ArithmeticException.class);
+        List<Integer> data = IntStream.iterate(1, i -> i + 1).limit(10).boxed().collect(Collectors.toList());
+        // try getting out element at index 1000 and set up consumers finalizers for success and failure
+        Try.of(() -> List.of(1, 2).get(1000)).endWith(n -> System.out.println("got " + n), err -> System.out.println("failed due to " + err.getMessage()));
 
     }
 }
